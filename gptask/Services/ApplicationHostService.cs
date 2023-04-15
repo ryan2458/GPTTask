4a083be33d0445b6bcf3682b4832efcd83ecd206
Ryan Aloof <r-aloof@outlook.com>
2023-04-14 18:39:03 -0700

    Added clarifying comment. Changed initial page we navigate to.

--- gptask/Services/ApplicationHostService.cs
+++ gptask/Services/ApplicationHostService.cs
@@ -51,7 +51,7 @@ namespace gptask.Services
                 _navigationWindow = (_serviceProvider.GetService(typeof(INavigationWindow)) as INavigationWindow)!;
                 _navigationWindow!.ShowWindow();
 
-                _navigationWindow.Navigate(typeof(Views.Pages.DashboardPage));
+                _navigationWindow.Navigate(typeof(Views.Pages.TaskListPage));
             }
 
             await Task.CompletedTask;
