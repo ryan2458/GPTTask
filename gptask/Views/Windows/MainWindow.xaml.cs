cab504422ff53342910dd9c21f42d880ed5f5346
Ryan Aloof <r-aloof@outlook.com>
2023-04-17 00:53:35 -0700

    Added comments.

--- gptask/Views/Windows/MainWindow.xaml.cs
+++ gptask/Views/Windows/MainWindow.xaml.cs
@@ -20,15 +20,28 @@ namespace gptask.Views.Windows
     /// </summary>
     public partial class MainWindow : INavigationWindow
     {
+        /// <summary>
+        /// A reference to the database.
+        /// </summary>
         private IDataService _dataService;
 
+        /// <summary>
+        /// A reference to the task list page.
+        /// This is where our TaskListItemModel item information is displayed.
+        /// </summary>
         private TaskListPage _taskListPage;
 
+        /// <summary>
+        /// Gets or sets the view model.
+        /// </summary>
         public ViewModels.MainWindowViewModel ViewModel
         {
             get;
         }
 
+        /// <summary>
+        /// Constructs a new MainWindow
+        /// </summary>
         public MainWindow(ViewModels.MainWindowViewModel viewModel, IPageService pageService, 
             INavigationService navigationService,
             IDataService dataService,
@@ -97,12 +110,20 @@ namespace gptask.Views.Windows
             Application.Current.Shutdown();
         }
 
+        /// <summary>
+        /// Raised when the AddList button is clicked.
+        /// Brings up a textbox for a new list to be created.
+        /// </summary>
         private void AddListButton_Click(object sender, RoutedEventArgs e)
         {
             AddListPopup.IsOpen = true;
             AddListTextBox.Focus();
         }
 
+        /// <summary>
+        /// Raised when a key is pressed in the AddListTextBox control when adding a list.
+        /// Only succeeds if the user presses enter.
+        /// </summary>
         private void AddListTextBox_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
@@ -135,28 +156,55 @@ namespace gptask.Views.Windows
             }
         }
 
+        /// <summary>
+        /// Raised when the textbox for creating a list loses focus.
+        /// </summary>
         private void AddListTextBox_LostFocus(object sender, RoutedEventArgs e)
         {
             AddListPopup.IsOpen = false;
             AddListTextBox.Clear();
         }
 
+        /// <summary>
+        /// Deletes a list.
+        /// </summary>
+        /// <param name="sender">the sender of the delete list click event.</param>
+        /// <param name="e">event args</param>
+        /// <param name="listModel">the list model associated with this list.</param>
+        /// <exception cref="NotImplementedException"></exception>
         private void DeleteListClick(object sender, RoutedEventArgs e, ListModel listModel)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Edits a list's name.
+        /// </summary>
+        /// <param name="sender">the sender of the edit list click event.</param>
+        /// <param name="e">event args</param>
+        /// <param name="listModel">the list model associated with this list.</param>
+        /// <exception cref="NotImplementedException"></exception>
         private void EditListClick(object sender, RoutedEventArgs e, ListModel listModel)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Handles navigation for when a list item is clicked.
+        /// </summary>
+        /// <param name="sender">the sender of the click event.</param>
+        /// <param name="e">event args, should be the navigation item.</param>
         private void Item_Click(object sender, RoutedEventArgs e)
         {
             NavigationItem item = e.Source as NavigationItem;
             RootNavigation.Navigate(item.PageTag);
         }
 
+        /// <summary>
+        /// Sets up a navigation for full functionality.
+        /// </summary>
+        /// <param name="item">The navigation item we're setting up.</param>
+        /// <param name="listModel">The list model that corresponds to this navigation item.</param>
         private void SetupNavigationItem(NavigationItem item, ListModel listModel)
         {
             item.Click += Item_Click;
